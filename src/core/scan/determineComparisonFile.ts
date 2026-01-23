37702ba05a58f741783d3ebc232867ca9be8bb6e
Chrilleweb <christian.munknissen@gmail.com>
2026-01-23 14:15:25 +0100

    moved folder

--- src/core/scan/determineComparisonFile.ts
+++ src/core/scan/determineComparisonFile.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
-import type { ScanUsageOptions } from '../config/types.js';
-import { resolveFromCwd } from './helpers/resolveFromCwd.js';
-import { DEFAULT_ENV_CANDIDATES } from '../config/constants.js';
+import type { ScanUsageOptions } from '../../config/types.js';
+import { resolveFromCwd } from '../helpers/resolveFromCwd.js';
+import { DEFAULT_ENV_CANDIDATES } from '../../config/constants.js';
 
 type ComparisonFile = {
   path: string;
